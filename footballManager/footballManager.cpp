fcee8f8db9661fc66086e9d07eb253156eb4181e
jamie274 <jamie-lee.gordon@city.ac.uk>
2022-09-08 15:55:36 +0100

    game finished, all rounds completed and tested

--- footballManager/footballManager.cpp
+++ footballManager/footballManager.cpp
@@ -171,7 +171,7 @@ string winner(string teamA, string teamB, int num) {
         winner = teamB;
     }
 
-    return winner;
+    return winner;  
 }
 
 int team1Goals;
@@ -194,7 +194,8 @@ int team16Goals;
 void gameOver(string team1, string team2) {
     int choice;
 
-    cout << "        You have been knocked out by " << team2 << " losing " << team1Goals << " - " << team2Goals << endl << endl;
+    cout << endl;
+    cout << "        You have been knocked out by " << team2 << " losing " << team1Goals << " - " << team2Goals << " on aggregate" << endl << endl;
 
     m:
     cout << "      Press 1 to PLAY AGAIN" << endl;
@@ -298,7 +299,12 @@ void roundOf16() {
     int num7 = rand() % v.size();
     int num8 = rand() % v.size();
 
-    winners.push_back(winner(team1,team2,num1));
+    if (winner(team1, team2, num1) == team1 ) {
+    }
+    else {
+        winners.push_back(winner(team1, team2, num1));
+    }
+    
     winners.push_back(winner(team3, team4,num2));
     winners.push_back(winner(team5, team6,num3));
     winners.push_back(winner(team7, team8,num4));
@@ -391,6 +397,8 @@ void roundOf16() {
     cin >> c;
 
     if (c == 1) {
+        cout << endl;
+        cout << "    AGGREGATE SCORES (2 legs):" << endl << endl;
         cout << "    " << team1 << " " << team1Goals << " - " << team2Goals << " " << team2 << endl;
         cout << "    " << team3 << " " << team3Goals << " - " << team4Goals << " " << team4 << endl;
         cout << "    " << team5 << " " << team5Goals << " - " << team6Goals << " " << team6 << endl;
@@ -418,8 +426,6 @@ void roundOf16() {
     else {
         goto x;
     }
-
-    
 }
 
 
@@ -427,13 +433,311 @@ void quaterFinals() {
     cout << endl;
     cout << "        You have qualified for the quater finals" << endl << endl;
     cout << "        QUATER FINALS" << endl << endl;
+
+    vector<string> winners;
+
+    string upperName;
+    for (char c : Team::teamName) {
+        upperName += toupper(c);
+    }
+    Team::teamName = upperName;
+
+    // setting up matches
+    // match 1
+    string team1 = Team::teamName;
+    string team2 = randTeam();
+    remTeam(team2);
+
+    // match 2
+    string team3 = randTeam();
+    remTeam(team3);
+    string team4 = randTeam();
+    remTeam(team4);
+
+    // match 3
+    string team5 = randTeam();
+    remTeam(team5);
+    string team6 = randTeam();
+    remTeam(team6);
+
+    // match 4
+    string team7 = randTeam();
+    remTeam(team7);
+    string team8 = randTeam();
+    remTeam(team8);
+
+    cout << "    Upcoming fixtures: " << endl << endl;
+    cout << "    " + team1 + " vs " + team2 << endl;
+    cout << "    " + team3 + " vs " + team4 << endl;
+    cout << "    " + team5 + " vs " + team6 << endl;
+    cout << "    " + team7 + " vs " + team8 << endl;
+    
+    vector<int> v{ 'w','l' };
+    srand(time(NULL));
+
+    int num1 = rand() % v.size();
+    int num2 = rand() % v.size();
+    int num3 = rand() % v.size();
+    int num4 = rand() % v.size();
+    
+    if (winner(team1, team2, num1) == team1) {
+    }
+    else {
+        winners.push_back(winner(team1, team2, num1));
+    }
+
+    winners.push_back(winner(team3, team4, num2));
+    winners.push_back(winner(team5, team6, num3));
+    winners.push_back(winner(team7, team8, num4));
+    
+    Team::teamsRemaining = winners;
+
+    if (winner(team1, team2, num1) == team1) {
+        team1Goals = rand() % 8 + 1;
+        team2Goals = rand() % team1Goals;
+    }
+    else {
+        team2Goals = rand() % 8 + 1;
+        team1Goals = rand() % team2Goals;
+    }
+
+    if (winner(team3, team4, num2) == team3) {
+        team3Goals = rand() % 8 + 1;
+        team4Goals = rand() % team3Goals;
+    }
+    else {
+        team4Goals = rand() % 8 + 1;
+        team3Goals = rand() % team4Goals;
+    }
+
+    if (winner(team5, team6, num3) == team5) {
+        team5Goals = rand() % 8 + 1;
+        team6Goals = rand() % team5Goals;
+    }
+    else {
+        team6Goals = rand() % 8 + 1;
+        team5Goals = rand() % team6Goals;
+    }
+
+    if (winner(team7, team8, num4) == team7) {
+        team7Goals = rand() % 8 + 1;
+        team8Goals = rand() % team7Goals;
+    }
+    else {
+        team8Goals = rand() % 8 + 1;
+        team7Goals = rand() % team8Goals;
+    }
+
+    int c;
+
+    x:
+    cout << endl;
+    cout << "    Press 1 to simulate results" << endl;
+    cout << "    Press 2 to start again" << endl;
+    cout << "    Press 3 to EXIT" << endl;
+    cin >> c;
+
+    if (c == 1) {
+        cout << "    AGGREGATE SCORES (2 legs):" << endl << endl;
+        cout << "    " << team1 << " " << team1Goals << " - " << team2Goals << " " << team2 << endl;
+        cout << "    " << team3 << " " << team3Goals << " - " << team4Goals << " " << team4 << endl;
+        cout << "    " << team5 << " " << team5Goals << " - " << team6Goals << " " << team6 << endl;
+        cout << "    " << team7 << " " << team7Goals << " - " << team8Goals << " " << team8 << endl;
+       
+        // game is over if you get knocked out
+        if (team1Goals < team2Goals) {
+            gameOver(team1, team2);
+        }
+        else {
+            semiFinals();
+        }
+    }
+    else if (c == 2) {
+        system("cls");
+        Team t;
+        t.selectTeam();
+    }
+    else if (c == 3) {
+        exit(0);
+    }
+    else {
+        goto x;
+    }
+
+
 }
 void semiFinals() {
+    cout << endl;
+    cout << "        You have qualified for the semi finals" << endl << endl;
+    cout << "        SEMI FINALS" << endl << endl;
+
+    vector<string> winners;
+
+    string upperName;
+    for (char c : Team::teamName) {
+        upperName += toupper(c);
+    }
+    Team::teamName = upperName;
+
+    // setting up matches
+    // match 1
+    string team1 = Team::teamName;
+    string team2 = randTeam();
+    remTeam(team2);
+
+    // match 2
+    string team3 = randTeam();
+    remTeam(team3);
+    string team4 = randTeam();
+    remTeam(team4);
+
+    cout << "    Upcoming fixtures: " << endl << endl;
+    cout << "    " + team1 + " vs " + team2 << endl;
+    cout << "    " + team3 + " vs " + team4 << endl;
+  
+    vector<int> v{ 'w','l' };
+    srand(time(NULL));
+
+    int num1 = rand() % v.size();
+    int num2 = rand() % v.size();
+
+    if (winner(team1, team2, num1) == team1) {
+    }
+    else {
+        winners.push_back(winner(team1, team2, num1));
+    }
+
+    winners.push_back(winner(team3, team4, num2));
+
+    Team::teamsRemaining = winners;
+
+    if (winner(team1, team2, num1) == team1) {
+        team1Goals = rand() % 8 + 1;
+        team2Goals = rand() % team1Goals;
+    }
+    else {
+        team2Goals = rand() % 8 + 1;
+        team1Goals = rand() % team2Goals;
+    }
+
+    if (winner(team3, team4, num2) == team3) {
+        team3Goals = rand() % 8 + 1;
+        team4Goals = rand() % team3Goals;
+    }
+    else {
+        team4Goals = rand() % 8 + 1;
+        team3Goals = rand() % team4Goals;
+    }
+
+    int c;
+
+    x:
+    cout << endl;
+    cout << "    Press 1 to simulate results" << endl;
+    cout << "    Press 2 to start again" << endl;
+    cout << "    Press 3 to EXIT" << endl;
+    cin >> c;
+
+    if (c == 1) {
+        cout << "    AGGREGATE SCORES (2 legs):" << endl << endl;
+        cout << "    " << team1 << " " << team1Goals << " - " << team2Goals << " " << team2 << endl;
+        cout << "    " << team3 << " " << team3Goals << " - " << team4Goals << " " << team4 << endl;
+
+        // game is over if you get knocked out
+        if (team1Goals < team2Goals) {
+            gameOver(team1, team2);
+        }
+        else {
+            final();
+        }
+    }
+    else if (c == 2) {
+        system("cls");
+        Team t;
+        t.selectTeam();
+    }
+    else if (c == 3) {
+        exit(0);
+    }
+    else {
+        goto x;
+    }
 
 }
 void final() {
+    cout << endl;
+    cout << "        You have qualified for the semi finals" << endl << endl;
+    cout << "        SEMI FINALS" << endl << endl;
+
+    vector<string> winners;
+
+    string upperName;
+    for (char c : Team::teamName) {
+        upperName += toupper(c);
+    }
+    Team::teamName = upperName;
+
+    string team1 = Team::teamName;
+    string team2 = randTeam();
+    remTeam(team2);
+
+    cout << "    Upcoming fixtures: " << endl << endl;
+    cout << "    " + team1 + " vs " + team2 << endl;
+
+    vector<int> v{ 'w','l' };
+    srand(time(NULL));
+
+    int num1 = rand() % v.size();
+
+    winners.push_back(winner(team1, team2, num1));
+
+    Team::teamsRemaining = winners;
+
+    if (winner(team1, team2, num1) == team1) {
+        team1Goals = rand() % 8 + 1;
+        team2Goals = rand() % team1Goals;
+    }
+    else {
+        team2Goals = rand() % 8 + 1;
+        team1Goals = rand() % team2Goals;
+    }
+
+    int c;
+
+    x:
+    cout << endl;
+    cout << "    Press 1 to simulate results" << endl;
+    cout << "    Press 2 to start again" << endl;
+    cout << "    Press 3 to EXIT" << endl;
+    cin >> c;
+
+    if (c == 1) {
+        cout << "    FINAL SCORE" << endl << endl;
+        cout << "    " << team1 << " " << team1Goals << " - " << team2Goals << " " << team2 << endl;
+        
+        // game is over if you get knocked out
+        if (team1Goals < team2Goals) {
+            gameOver(team1, team2);
+        }
+        else {
+            cout << "    CONGRATULATIONS you have won this years Champions league" << endl << endl;
+            cout << "    " + Team::teamName + " CHAMPIONS" << endl << endl;
+        }
+    }
+    else if (c == 2) {
+        system("cls");
+        Team t;
+        t.selectTeam();
+    }
+    else if (c == 3) {
+        exit(0);
+    }
+    else {
+        goto x;
+    }
 
 }
+
 
 void result(string team1, string team2) {
 
